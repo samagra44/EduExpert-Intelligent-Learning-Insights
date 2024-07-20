504f9eae6b72998a849341b0dc5eb66492c66634
samagra44 <samagra44@gmail.com>
2024-07-20 19:52:09 +0530

    error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.environ['GRADIENT_ACCESS_TOKEN'] = "crU9EfAP4C7Eikc4GHGAnMlM75S18KHW"
 os.environ['GRADIENT_WORKSPACE_ID'] = "e7fe2f17-3bac-4123-8bd7-2f5dccde400e_workspace"
 
 # Define the Dataset Path
-course_dataset_path = "data\course_data.csv"
+course_dataset_path = "course_data.csv"
 
 # Initialize the Gradient
 gradient = Gradient()
